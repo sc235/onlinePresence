301f52269f1c0ab8f83c6d4aca89158c52c51cae
ahmed ndiaye <ahmedndiaye@gmail.com>
2026-07-06 15:03:15 +0000

    deploy: add support for Railway automatic postgres variables

--- backend/src/datasources/db.datasource.ts
+++ backend/src/datasources/db.datasource.ts
@@ -9,12 +9,12 @@ dotenv.config({path: path.join(__dirname, '../../.env')});
 const config = {
   name: 'db',
   connector: 'postgresql',
-  url: '',
-  host: process.env.DB_HOST ?? '127.0.0.1',
-  port: +(process.env.DB_PORT ?? 5432),
-  user: process.env.DB_USER ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? 'postgres',
-  database: process.env.DB_DATABASE ?? 'cabinet_avocat',
+  url: process.env.DATABASE_URL ?? '',
+  host: process.env.DB_HOST ?? process.env.PGHOST ?? '127.0.0.1',
+  port: +(process.env.DB_PORT ?? process.env.PGPORT ?? 5432),
+  user: process.env.DB_USER ?? process.env.PGUSER ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? process.env.PGPASSWORD ?? 'postgres',
+  database: process.env.DB_DATABASE ?? process.env.PGDATABASE ?? 'rdv',
 };
 
 // Observe application life cycle to be notified when database connects/disconnects
